3cd905bbd791f1b8656cf0502e54a65cb2ff323e
roccogiordano <rocco.giordano573@gmail.com>
2024-12-03 17:04:09 +0100

    feat: added Product class

---
+++ java-inheritance-shop/src/org/lessons/java/inheritance/Product.java
@@ -0,0 +1,56 @@
+package org.lessons.java.inheritance;
+
+import java.util.Random;
+
+// Class Definition
+
+public class Product {
+
+    // Instance Variables Declaration
+
+    int code;
+    String name;
+    String description;
+    float basePrice;
+    float totalPrice;
+    int iva;
+
+    //
+
+
+    // Product Methods
+
+    Random randomCode = new Random();
+
+    public Product (String name, String description, float basePrice, int iva) {
+
+        this.code = randomCode.nextInt(999999);
+
+        this.name = name;
+
+        this.description = description;
+
+        this.basePrice = basePrice;
+
+        this.iva = iva;
+
+        this.totalPrice = basePrice + (basePrice / 100 * iva);
+
+    }
+
+    public String getBasePrice() {
+        return("Price before taxes is: " + basePrice);
+    }
+
+    public String getTotalPrice() {
+        return("Price after taxes will be: " + totalPrice);
+    }
+
+    public String getFullName() {
+        return("Product full name is: " + this.code + "-" + this.name);
+    }
+
+    //
+}
+
+//
