640954910fe0ddce6738ced5c85a11e956d58377
roccogiordano <rocco.giordano573@gmail.com>
2024-12-03 17:16:48 +0100

    feat: added getter and setter methods to Product class

--- java-inheritance-shop/src/org/lessons/java/inheritance/Product.java
+++ java-inheritance-shop/src/org/lessons/java/inheritance/Product.java
@@ -38,12 +38,44 @@ public class Product {
 
     }
 
+    public String getCode() {
+        return("Product code is: " + this.code);
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return("Product name is: " + this.name);
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getBrand() {
+        return("Product brand is: " + this.brand);
+    }
+
+    public void setBasePrice(int basePrice) {
+        this.basePrice = basePrice;
+    }
+
     public String getBasePrice() {
-        return("Price before taxes is: " + basePrice);
+        return("Price before taxes is: " + this.basePrice);
+    }
+
+    public void setIva(int iva) {
+        this.iva = iva;
+    }
+
+    public String getIva() {
+        return("Product IVA is: " + this.iva);
     }
 
     public String getTotalPrice() {
-        return("Price after taxes will be: " + totalPrice);
+        return("Price after taxes will be: " + this.totalPrice);
     }
 
     //
