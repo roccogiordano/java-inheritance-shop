1de2d3cb406ed984a25e6ce7ee313a70733610af
roccogiordano <rocco.giordano573@gmail.com>
2024-12-10 17:34:56 +0100

    feat: added Processor and Screen classes

---
+++ java-inheritance-shop/src/org/lessons/java/inheritance/Processor.java
@@ -0,0 +1,29 @@
+package org.lessons.java.inheritance;
+
+
+// Processor Class Definition
+
+public class Processor {
+
+    // Instance Variables Declaration
+
+    private String model;
+    private int nanometers;
+
+    //
+
+
+    // Processor Methods
+
+    public Processor(String model, int nanometers) {
+
+        this.model = model;
+        this.nanometers = nanometers;
+        
+    }
+
+    //
+    
+}
+
+//
---
+++ java-inheritance-shop/src/org/lessons/java/inheritance/Screen.java
@@ -0,0 +1,29 @@
+package org.lessons.java.inheritance;
+
+
+// Processor Class Definition
+
+public class Screen {
+
+    // Instance Variables Declaration
+
+    private String screenType;
+    private String maxResolution;
+
+    //
+
+
+    // Processor Methods
+
+    public Screen(String screenType, String maxResolution) {
+
+        this.screenType = screenType;
+        this.maxResolution = maxResolution;
+        
+    }
+
+    //
+    
+}
+
+//
